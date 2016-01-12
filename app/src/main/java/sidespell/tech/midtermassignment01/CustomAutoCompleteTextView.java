e59df6a1ad2a59818f751fa04b368e9f96d6320b
chanferolino <chanferolino@gmail.com>
2016-01-12 10:50:46 +0800

    First commit

---
+++ app/src/main/java/sidespell/tech/midtermassignment01/CustomAutoCompleteTextView.java
@@ -0,0 +1,29 @@
+package sidespell.tech.midtermassignment01;
+
+/**
+ * Created by Christian on 1/12/2016.
+ */
+
+import java.util.HashMap;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.AutoCompleteTextView;
+
+/** Customizing AutoCompleteTextView to return Country Name
+ *  corresponding to the selected item
+ */
+public class CustomAutoCompleteTextView extends AutoCompleteTextView {
+
+    public CustomAutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /** Returns the country name corresponding to the selected item */
+    @Override
+    protected CharSequence convertSelectionToString(Object selectedItem) {
+        /** Each item in the autocompetetextview suggestion list is a hashmap object */
+        HashMap<String, String> hm = (HashMap<String, String>) selectedItem;
+        return hm.get("txt");
+    }
+}
